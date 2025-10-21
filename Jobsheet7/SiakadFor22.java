db0f1cf6c7166dcce5fa822223b037eb31cc7a6e
bangadjie <masadjie927@gmail.com>
2025-10-22 02:46:12 +0700

    Modifikasi percobaan 1

--- Jobsheet7/SiakadFor22.java
+++ Jobsheet7/SiakadFor22.java
@@ -1,13 +1,13 @@
-import java.*;
 import java.util.Scanner;
 public class SiakadFor22 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
         double nilai, tertinggi = 0, terendah = 100;
+        int jmlLulus = 0, jmlTdkLulus = 0;
 
         for (int i = 1; i <= 10; i++) {
-            System.out.println("Masukkan nilai mahasiswa ke-" + i+ ": ");
+            System.out.print("Masukkan nilai mahasiswa ke-" + i+ ": ");
             nilai = sc.nextDouble();
             if (nilai > tertinggi) {
                 tertinggi = nilai;
@@ -15,9 +15,18 @@ public class SiakadFor22 {
             if (nilai < terendah) {
                 terendah = nilai;
             }
+            if(nilai >= 60){
+                System.out.println("LUlus");
+                jmlLulus++;
+            }else{
+                System.out.println("Tidak Lulus");
+                jmlTdkLulus++;
+            }
         }
 
         System.out.println("Nilai tertinggi: " + tertinggi);
         System.out.println("Nilai terendah: " + terendah);
+        System.out.println("Berikut untuk anak yang lulus: " + jmlLulus);
+        System.out.println("Berikut untuk anak yang tidak lulus: " + jmlTdkLulus);
     }
 }
