0f7b99249cbb7f26d9ba803c3078e5dff41ba604
bangadjie <masadjie927@gmail.com>
2025-10-31 05:58:57 +0700

    Percobaan 1

---
+++ Jobsheet8/Start22.java
@@ -0,0 +1,15 @@
+package Jobsheet8;
+import java.util.Scanner;
+public class Start22 {
+    public static void main(String[] args) {
+       Scanner sc = new Scanner(System.in);
+        
+        System.out.print("Masukkan nilai n: ");
+        int n = sc.nextInt();
+        
+        for(int i = 1; i<=n; i++){
+            System.out.print("*");
+        }
+    }
+}
+
