ca788bcd2a38d5b4ddf258ec7f311e54075f7d00
bangadjie <masadjie927@gmail.com>
2025-10-31 14:09:19 +0700

    Percobaan 1

--- Jobsheet8/Square.java
+++ Jobsheet8/Square.java
@@ -5,10 +5,10 @@ public class Square {
     public static void main(String[] args) {
        Scanner sc = new Scanner(System.in);
         
-        System.out.print("Masukkan nilai n: ");
+        System.out.print("Masukkan nilai N: ");
         int N = sc.nextInt();
         
-        for (int iOuter = 0; iOuter <= N; iOuter++) {
+        for (int iOuter = 1; iOuter <= N; iOuter++) {
             for(int i = 1; i<=N; i++){
             System.out.print("*");
         }
