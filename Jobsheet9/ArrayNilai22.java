8552190451388a39eada7466c95d36fd4190aae4
bangadjie <masadjie927@gmail.com>
2025-11-04 11:12:13 +0700

    Modifikasi Percobaan 2

--- Jobsheet9/ArrayNilai22.java
+++ Jobsheet9/ArrayNilai22.java
@@ -11,8 +11,9 @@ public class ArrayNilai22{
         for(int i = 0; i < nilaiAkhir.length; i++) {
             if (nilaiAkhir[i] > 70) {
                 System.out.println("Mahasiswa ke-"+i+" lulus!");
+            }else{
+                System.out.println("Mahasiswa ke-"+i+" tidak lulus!");
             }
-            // System.out.println("Nilai akhir ke-"+i+" adalah "+nilaiAkhir[i]);
         }
 
     }
