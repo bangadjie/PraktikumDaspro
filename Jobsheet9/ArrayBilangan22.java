ff60c5194cfc396ed651b66553717bf5bd0d76d7
bangadjie <masadjie927@gmail.com>
2025-11-04 10:41:17 +0700

    Modifikasi Percobaan 1

--- Jobsheet9/ArrayBilangan22.java
+++ Jobsheet9/ArrayBilangan22.java
@@ -7,9 +7,8 @@ public class ArrayBilangan22{
         bil[2] = -7;
         bil[3] = 17;
 
-        System.out.println(bil [1]);
-        System.out.println(bil [2]);
-        System.out.println(bil [3]);
-        System.out.println(bil [0]);
+        for(int i = 0; i < 4; i++) {
+            System.out.println(bil[i]);
+        }
     }
 }
