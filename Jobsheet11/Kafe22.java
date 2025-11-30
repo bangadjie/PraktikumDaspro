1723f68e0dfff9829711e8765f2981274d041ecb
bangadjie <masadjie927@gmail.com>
2025-11-30 17:21:43 +0700

    Percobaan 2

--- Jobsheet11/Kafe22.java
+++ Jobsheet11/Kafe22.java
@@ -1,6 +1,12 @@
 package Jobsheet11;
 public class Kafe22 {
-    public static void Menu(){
+    public static void Menu(String namaPelanggan, boolean isMember){
+        System.out.println("Selamat datang " + namaPelanggan + "!");
+
+        if (isMember) {
+            System.out.println("Anda adalah member, dapatkan diskon 10% untuk setiap pembelian!");
+        }
+
         System.out.println("=====MENU RESYO KAFE=====");
         System.out.println("1. Kopi Hitam - Rp 15,00");
         System.out.println("2. Cappuccino - Rp 20,000");
@@ -12,6 +18,6 @@ public class Kafe22 {
         System.out.println("Silahkan pilih menu yang Anda inginkan.");
     }
     public static void main(String[] args) {
-        Menu();
+        Menu("Andi", false);
     }
 }
