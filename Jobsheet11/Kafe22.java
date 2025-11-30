5668700f3a44605975b44d2424412ee1139a80a1
bangadjie <masadjie927@gmail.com>
2025-11-30 17:18:23 +0700

    Percobaan 1

---
+++ Jobsheet11/Kafe22.java
@@ -0,0 +1,17 @@
+package Jobsheet11;
+public class Kafe22 {
+    public static void Menu(){
+        System.out.println("=====MENU RESYO KAFE=====");
+        System.out.println("1. Kopi Hitam - Rp 15,00");
+        System.out.println("2. Cappuccino - Rp 20,000");
+        System.out.println("3. Latte - Rp 22,000");
+        System.out.println("4. Teh Tarik - Rp 12,000");
+        System.out.println("5. Roti Bakar - Rp 10,000");
+        System.out.println("6. Mie Goreng - Rp 18,000");
+        System.out.println("===========================");
+        System.out.println("Silahkan pilih menu yang Anda inginkan.");
+    }
+    public static void main(String[] args) {
+        Menu();
+    }
+}
